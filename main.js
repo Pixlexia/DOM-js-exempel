af239553c9b9889526970e3b714b6f703ec3f227
Michaela <michaelamalis@hotmail.com>
2022-11-25 11:12:50 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+function color() {
+    const changeColor = document.getElementById("rubrik")
+    changeColor.style.color = "pink"
+}
+
+function text() {
+    const changeText = document.getElementById("rubrik")
+    changeText.innerText = "Spana in våran epic JUL - REA!"
+}
+
+function multi() {
+    color()
+    text()
+}
